5823f1ac499fd85a45d26653451bebe513cbfaab
ved <vedantkotkar111@gmail.com>
2022-10-15 16:25:15 +0530

    just checking

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [search,setSearch] =useState('');
   // const [filtredCoins,setFiltredCoins] = useState(coins);
   // const [error,setError] = useEffect(false);
+  //thisline is added later
+  
 
   useEffect(()=>{
     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false")
